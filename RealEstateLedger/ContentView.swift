f847d79cc36bea0f6d58721410ebb0cba58d2c39
Wanbok Choi <wanbok.choi@linecorp.com>
2021-02-04 00:11:40 +0900

    Initial project

---
+++ RealEstateLedger/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RealEstateLedger
+//
+//  Created by Wanbok Choi on 2021/02/04.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RealEstateLedger/RealEstateLedgerApp.swift
@@ -0,0 +1,17 @@
+//
+//  RealEstateLedgerApp.swift
+//  RealEstateLedger
+//
+//  Created by Wanbok Choi on 2021/02/04.
+//
+
+import SwiftUI
+
+@main
+struct RealEstateLedgerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
